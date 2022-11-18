b217d77c5c5eb52adbe34a04077e151a5158d378
‘Anastasiia <miss.iuferova@yandex.ru>
2022-11-18 17:48:55 +0100

    feat: download

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,39 @@
+//Constants
+
 const headerMenuButton = document.getElementsByClassName("header__menu")[0];
-console.log(headerMenuButton)
-
 const headerMenuPopup = document.getElementsByClassName("menu__popup")[0];
+const popupNav = headerMenuPopup.getElementsByTagName("li");
 
+
+//Functions
 const openPopup = function ()  {
-    headerMenuButton.classList.toggle("header__menu_open");
-    headerMenuPopup.classList.toggle("menu__popup_opened");
+    headerMenuButton.classList.add("header__menu_open");
+    headerMenuPopup.classList.add("menu__popup_opened");
 }
 
-headerMenuButton.addEventListener("click", openPopup)
+const closePopup = function ()  {
+    headerMenuButton.classList.remove("header__menu_open");
+    headerMenuPopup.classList.remove("menu__popup_opened");
+}
 
-//screenwidth
+const popupToggle = function () {
+    if(!Array.from(headerMenuPopup.classList).includes("menu__popup_opened")) {
+        return openPopup()
+    }
+    else return  closePopup()
+}
+
+function resizeFunction() {
+    if (screen.width >= 900){
+        closePopup()
+    }
+}
+
+//EventListeners
+
+Array.from(popupNav).forEach(item => item.addEventListener("click", closePopup))
+
+headerMenuButton.addEventListener("click", popupToggle)
+
+window.addEventListener("resize", resizeFunction);
+
